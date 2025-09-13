a093cb3074df481ec36f33e5b4ee133318691b4c
Nattapong Promthong <banker.z412@gmail.com>
2025-09-13 23:37:59 +0700

    feat: use safelist to render class from remote

--- host/tailwind.config.js
+++ host/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
-    "../remote/src/**/*.{html,ts}"
+    "./src/**/*.{html,ts}"
+  ],
+  safelist: [
+    // รักษา classes ที่ remote อาจใช้
+    {
+      pattern: /bg-(red|green|blue|yellow|purple|pink|indigo)-(50|100|200|300|400|500|600|700|800|900)/,
+    },
+    {
+      pattern: /text-(red|green|blue|yellow|purple|pink|indigo)-(50|100|200|300|400|500|600|700|800|900)/,
+    },
+    {
+      pattern: /p-(0|1|2|3|4|5|6|8|10|12|16|20|24)/,
+    },
+    {
+      pattern: /m-(0|1|2|3|4|5|6|8|10|12|16|20|24)/,
+    },
+    // เพิ่ม patterns ตามที่ต้องการ
   ],
   theme: {
     extend: {},
