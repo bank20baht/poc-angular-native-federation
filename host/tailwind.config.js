13c0909347ae40e2ed5a2197ab23d1659ecf3253
Nattapong Promthong <banker.z412@gmail.com>
2025-09-13 23:39:45 +0700

    feat: use safelist to render class from remote (regex all class posible)

--- host/tailwind.config.js
+++ host/tailwind.config.js
@@ -4,20 +4,9 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   safelist: [
-    // รักษา classes ที่ remote อาจใช้
     {
-      pattern: /bg-(red|green|blue|yellow|purple|pink|indigo)-(50|100|200|300|400|500|600|700|800|900)/,
+      pattern: /([a-zA-Z]+)-./,
     },
-    {
-      pattern: /text-(red|green|blue|yellow|purple|pink|indigo)-(50|100|200|300|400|500|600|700|800|900)/,
-    },
-    {
-      pattern: /p-(0|1|2|3|4|5|6|8|10|12|16|20|24)/,
-    },
-    {
-      pattern: /m-(0|1|2|3|4|5|6|8|10|12|16|20|24)/,
-    },
-    // เพิ่ม patterns ตามที่ต้องการ
   ],
   theme: {
     extend: {},
