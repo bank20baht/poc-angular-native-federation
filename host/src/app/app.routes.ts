4005c65ab9fcc26db0dbb067442140d00be8ff5a
Nattapong Promthong <banker.z412@gmail.com>
2025-09-21 22:41:00 +0700

    feat: use remote components as page

--- host/src/app/app.routes.ts
+++ host/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      loadRemoteModule('remote', './Component').then((m) => m.App),
+  },
+];
